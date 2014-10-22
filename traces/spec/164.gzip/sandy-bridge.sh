2f41ff6a9e6f43bed34b5cd62161e062cb79e4d9
Dan Luu <danluu@gmail.com>
2014-10-21 19:43:03 -0500

    Add args to run sbc trace.

---
+++ traces/spec/164.gzip/sandy-bridge.sh
@@ -0,0 +1,3 @@
+../../../dineroIV -l1-isize 32768 -l1-iassoc 8 -l1-ibsize 64 -l1-irepl l -l1-dsize 32768 -l1-dassoc 8 -l1-dbsize 64 -l1-drepl l \
+-l2-uassoc 8 -l2-ubsize 64 -l2-usize 256k -l2-urepl l \
+-informat s -trname gzip_m2b -maxtrace 100

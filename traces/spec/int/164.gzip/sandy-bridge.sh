692d1f6bc9d8a97d4735742d9be368484a321f08
Dan Luu <danluu@gmail.com>
2014-10-25 20:52:09 -0500

    Fix cache size in script (must be power of 2).

--- traces/spec/int/164.gzip/sandy-bridge.sh
+++ traces/spec/int/164.gzip/sandy-bridge.sh
@@ -2,5 +2,5 @@
 ../../../../dineroIV -l1-isize 32k -l1-iassoc 8 -l1-ibsize 64 -l1-irepl l \
 -l1-dassoc 8 -l1-dbsize 64 -l1-dsize 32k  -l1-drepl l \
 -l2-uassoc 8 -l2-ubsize 64 -l2-usize 256k -l2-urepl l \
--l3-uassoc 8 -l3-ubsize 64 -l3-usize 768k   -l2-urepl l \
+-l3-uassoc 8 -l3-ubsize 64 -l3-usize 1m   -l2-urepl l \
 -informat s -trname gzip_m2b -maxtrace 1
